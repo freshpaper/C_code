d92f3b8e84aa7b917d1283a967729158ee782eab
freshpaper <102936506+freshpaper@users.noreply.github.com>
2022-04-12 17:18:42 +0800

    Add files via upload

---
+++ test1_2022_4_4/test1_2022_4_4/text1.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	char ch = 'A';
+//	printf("hello word\n");
+//	printf("hehe\n");
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+int num2 = 20;//全局变量
+int main()
+{
+	int num1 = 10;//局部变量
+	return 0;
+}
